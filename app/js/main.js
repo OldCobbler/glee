e6acefd8660b8dc00b1b569437aa3b1a47d098a2
OldCobbler <oldcobbler@yandex.ru>
2023-06-27 12:47:17 +0300

    added product-detalis page

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,42 @@
 $(function(){
 
+  $('.detalis-related__items').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1
+  });
+
+  $('.detalis-tabs__top-link').on('click', function(e){
+    e.preventDefault();
+    $('.detalis-tabs__top-link').removeClass('detalis-tabs__top-link--active');
+    $(this).addClass('detalis-tabs__top-link--active');
+
+    $('.detalis-tabs__content-item').removeClass('detalis-tabs__content-item--active');
+    $($(this).attr('href')).addClass('detalis-tabs__content-item--active');
+  });
+
+  $('.detalis-one__input').styler();
+
+  $(".detalis-one__item-star").rateYo({
+    starWidth: "18px",
+    readOnly: true,
+    ratedFill: "#ffcc00"
+  });
+
+  $('.detalis-slider__thumb').slick({
+    asNavFor: '.detalis-slider__big',
+    focusOnSelect: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    vertical: true,
+    draggable: false
+  });
+  $('.detalis-slider__big').slick({
+    asNavFor: '.detalis-slider__thumb',
+    draggable: false,
+    arrows: false,
+    fade: true
+  });
   
   $(".star").rateYo({
     starWidth: "12px",
